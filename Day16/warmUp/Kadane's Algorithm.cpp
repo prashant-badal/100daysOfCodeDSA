da1b215c71914394df0744088ed4eb8f4d06b028
Prashant Badal <p26badal@gmail.com>
2024-04-03 22:05:12 +0530

    Day16

---
+++ Day16/warmUp/Kadane's Algorithm.cpp
@@ -0,0 +1,17 @@
+// Kadane's Algorithm
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+  long long ans=INT_MIN, sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            ans=max(ans,sum);
+            if(sum<0){
+                sum=0;
+            }
+            
+        }
+        return ans;
+        
+    }
